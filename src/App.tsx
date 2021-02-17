831f7a597573cab0641ecb23b12555445359fdbc
Richard Ng <richardcrng@gmail.com>
2021-02-17 07:52:02 +0000

    Add reference examples for larger onClicks

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,33 @@
 /**
- * Main takeaway: we can also pass functions inline
- *  just as we can pass in a reference.
- *
- * N.B. Passing an inline function expression is unorthodox.
- *  We do not recommend that you do it. However, it works -
- *  because it is passing a function.
+ * Main takeaway: you can do lots of things inside an onClick's
+ *  function. Just pass in a function which does the things which
+ *  you want to happen onClick.
  */
 
 function App() {
+  // this should log a different number (almost) every time
+  const logRandomOneLine = () => console.log(Math.random());
+
+  const logRandomTwoLines = () => {
+    // multiple lines requires an arrow function with body
+    const randomNumber = Math.random();
+    console.log(randomNumber);
+  };
+
+  const alertAndLogRandomNumber = () => {
+    window.alert("About to log a random number!");
+    console.log(Math.random()); // could replace with either of the below
+    // logRandomOneLine()
+    // logRandomTwoLines()
+  };
+
   return (
     <div>
       <h1>My buttons</h1>
-      <button
-        // NB this is unorthodox, nobody does it - but it works,
-        //  because it's a function we're passing in
-        onClick={function handleClick() {
-          console.log("From named function expression");
-        }}
-      >
-        Inline named function expression
-      </button>
-      <button
-        // NB this is unorthodox, nobody does it - but it works,
-        //  because it's a function we're passing in
-        onClick={function () {
-          console.log("From anonymous function expression");
-        }}
-      >
-        Inline anonymous function expression
-      </button>
-      <button
-        onClick={() => {
-          console.log("From inline anonymous arrow function with body");
-        }}
-      >
-        Inline anonymous arrow function with body
-      </button>
-      {/*
-        Inline arrow function with implicit return is most common
-          for _simple_ onClicks, because it is the most concise
-          and fits easily on one line.
-
-        However, all the other ways of doing it work as well -
-          because they're all passing functions to the onClick.
-      */}
-      <button onClick={() => console.log("Most concise")}>
-        Inline anonymous arrow function, implicit return
+      <button onClick={logRandomOneLine}>Log random number (1)</button>
+      <button onClick={logRandomTwoLines}>Log random number (2)</button>
+      <button onClick={alertAndLogRandomNumber}>
+        Alert and log random number
       </button>
     </div>
   );
